ff5bc76c98228d8b734df3d46b9191d754d0ab0a
SarthakBhardwajCode <85218961+sarthakbhardwaj27@users.noreply.github.com>
2023-10-10 02:44:14 +0530

    it finally works

--- content.js
+++ content.js
@@ -1,21 +1,23 @@
 function convertEpochToHumanReadable(epoch) {
-  const date = new Date(epoch * 1000);
-  return date.toString();
-}
-
-function helper(event) {
+    const date = new Date(epoch * 1000);
+    return date.toString();
+  }
+  
+  function helper(event) {
+  
     if (event.ctrlKey) {
-        const selection = window.getSelection().toString().trim();
-    
-        // Trigger conversion only if the selection has more than 10 characters
-        if (selection.length > 10) {
-          const epochValue = parseInt(selection, 10);
-    
-          if (!isNaN(epochValue)) {
-            alert(`Human Readable: ${convertEpochToHumanReadable(epochValue)}`);
-          }
+      const selection = window.getSelection().toString().trim();
+  
+      // Trigger conversion only if the selection has more than 10 characters
+      if (selection.length >= 10) {
+        const epochValue = parseInt(selection, 10);
+  
+        if (!isNaN(epochValue)) {
+          alert(`Human Readable: ${convertEpochToHumanReadable(epochValue)}`);
         }
       }
-}
-
-document.addEventListener("mouseup", helper);
+    }
+  }
+  
+  document.addEventListener("mouseup", helper);
+  
